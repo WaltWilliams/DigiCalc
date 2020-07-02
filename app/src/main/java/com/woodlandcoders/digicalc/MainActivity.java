bf9236f694e23be6dfefd6471ed677d40ebcc30b
Walt Williams <red380sl@gmail.com>
2020-07-02 14:19:25 -0600

    Added code to test for Non SDK Interfaces.
    I can only test for Non SDK interfaces on my Intel based laptop.

--- app/src/main/java/com/woodlandcoders/digicalc/MainActivity.java
+++ app/src/main/java/com/woodlandcoders/digicalc/MainActivity.java
@@ -14,11 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-//        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-//            builder.detectNonSdkApiUsage();
-//        }
-//        StrictMode.setVmPolicy(builder.penaltyLog().build());
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            builder.detectNonSdkApiUsage();
+            StrictMode.setVmPolicy(builder.penaltyLog().build());
+        }
 
         setContentView(R.layout.activity_main);
 
